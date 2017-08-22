406fcb0bfa62769903c27e81dc4149f43547da01
Tobias Goulden Schultz <tobias.g.schultz@gmail.com>
2017-08-22 09:56:19 +0200

    Add breakpoint for mobile

--- dist/config.js
+++ dist/config.js
@@ -4,6 +4,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 var breakpoints = exports.breakpoints = {
+  xs: '(max-width: 40em)',
   sm: '(min-width: 40em)',
   md: '(min-width: 52em)',
   lg: '(min-width: 64em)'
